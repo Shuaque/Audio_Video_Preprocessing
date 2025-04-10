5b99d3163b32b972e068cb18d5ed673b06e60bb6
Shuak Bakytnur <shuakbakytnur@gmail.com>
2025-04-10 22:37:32 +0800

    Update create_scp.py

--- create_scp.py
+++ create_scp.py
@@ -36,6 +36,26 @@ def create_scp(scp_root, **data_dir):
                     f.write('\n')
                     
 
+def handle_scp(scp_file: str) -> dict:
+    '''
+        input: scp file
+        output: scp dictionary -> {'.wav':'absolute path'}
+    '''
+    scp_data = dict()
+
+    open with(scp_file, 'r') as f:
+        for line_num, line in enumerate(f, start=1):
+            scp_line = line.strip.split()
+            if len(scp_line) != 2:
+                raise RuntimeError(f"For {scp_file}, format error in line {line_num}: {scp_line}")
+            k,v = scp_line
+            if k in scp_data:
+                raise ValueError(f"Duplicated key '{k}' exists in line {line_num} on {scp_file}")
+            
+            scp_data[k] = v
+            
+    return scp_data
+    
 if __name__ == "__main__":
     
     #Directory where scp files are saved, end with '/'
@@ -55,3 +75,10 @@ if __name__ == "__main__":
                cv_s1 = '../wsj0-2mix/2speakers/wav8k/min/cv/s1',
                cv_s2 = '../wsj0-2mix/2speakers/wav8k/min/cv/s2',
                )
+
+    scp_file = '../data/train_mix.scp'
+    scp_dict = handle(scp_file)
+    print(type(scp_dict)）
+
+    for k, v in list(scp_dict.items())[:5]:
+    print(k,v)
